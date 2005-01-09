8bb53ad00cb4965b8784399a37d84434356a6130
Sushma Rai <rsushma@src.gnome.org>
2005-01-09 13:45:24 +0000

    new header file

---
+++ lib/e2k-validate.h
@@ -0,0 +1,18 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*- */
+/* Copyright (C) 2003, 2004 Novell, Inc. */
+
+#ifndef __E2K_VALIDATE_H_
+#define __E2K_VALIDATE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#pragma }
+#endif /* __cplusplus */
+
+gboolean e2k_validate_user (char *owa_url, char *user, char **host);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* __EXCHANGE_VALIDATE_H_ */
