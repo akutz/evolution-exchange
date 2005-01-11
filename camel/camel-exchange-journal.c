c53cd14bee578f4ea4ae3941cd3590cd1c002712
Ahmed Sarfraaz <sarfraaz@src.gnome.org>
2005-01-11 11:17:04 +0000

    Missed these out in my previous commit.

---
+++ camel/camel-exchange-journal.c
@@ -0,0 +1,393 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ *  Copyright 2004 Novell, Inc. (www.novell.com)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <errno.h>
+
+#include <camel/camel-i18n.h>
+#include <camel/camel-folder.h>
+#include <camel/camel-file-utils.h>
+#include <camel/camel-folder-summary.h>
+#include <camel/camel-data-cache.h>
+
+#include "camel-exchange-journal.h"
+
+
+#define d(x) x
+
+
+static void camel_exchange_journal_class_init (CamelExchangeJournalClass *klass);
+static void camel_exchange_journal_init (CamelExchangeJournal *journal, CamelExchangeJournalClass *klass);
+static void camel_exchange_journal_finalize (CamelObject *object);
+
+static void exchange_entry_free (CamelOfflineJournal *journal, EDListNode *entry);
+static EDListNode *exchange_entry_load (CamelOfflineJournal *journal, FILE *in);
+static int exchange_entry_write (CamelOfflineJournal *journal, EDListNode *entry, FILE *out);
+static int exchange_entry_play (CamelOfflineJournal *journal, EDListNode *entry, CamelException *ex);
+
+
+static CamelOfflineJournalClass *parent_class = NULL;
+
+
+CamelType
+camel_exchange_journal_get_type (void)
+{
+	static CamelType type = 0;
+	
+	if (!type) {
+		type = camel_type_register (camel_offline_journal_get_type (),
+					    "CamelExchangeJournal",
+					    sizeof (CamelExchangeJournal),
+					    sizeof (CamelExchangeJournalClass),
+					    (CamelObjectClassInitFunc) camel_exchange_journal_class_init,
+					    NULL,
+					    (CamelObjectInitFunc) camel_exchange_journal_init,
+					    (CamelObjectFinalizeFunc) camel_exchange_journal_finalize);
+	}
+	
+	return type;
+}
+
+static void
+camel_exchange_journal_class_init (CamelExchangeJournalClass *klass)
+{
+	CamelOfflineJournalClass *journal_class = (CamelOfflineJournalClass *) klass;
+	
+	parent_class = (CamelOfflineJournalClass *) camel_type_get_global_classfuncs (CAMEL_TYPE_OFFLINE_JOURNAL);
+	
+	journal_class->entry_free = exchange_entry_free;
+	journal_class->entry_load = exchange_entry_load;
+	journal_class->entry_write = exchange_entry_write;
+	journal_class->entry_play = exchange_entry_play;
+}
+
+static void
+camel_exchange_journal_init (CamelExchangeJournal *journal, CamelExchangeJournalClass *klass)
+{
+	
+}
+
+static void
+camel_exchange_journal_finalize (CamelObject *object)
+{
+	
+}
+
+static void
+exchange_entry_free (CamelOfflineJournal *journal, EDListNode *entry)
+{
+	CamelExchangeJournalEntry *exchange_entry = (CamelExchangeJournalEntry *) entry;
+	
+	g_free (exchange_entry->uid);
+	g_free (exchange_entry);
+}
+
+static EDListNode *
+exchange_entry_load (CamelOfflineJournal *journal, FILE *in)
+{
+	CamelExchangeJournalEntry *entry;
+	
+	entry = g_malloc0 (sizeof (CamelExchangeJournalEntry));
+	
+	if (camel_file_util_decode_uint32 (in, &entry->type) == -1)
+		goto exception;
+	
+	switch (entry->type) {
+	case CAMEL_EXCHANGE_JOURNAL_ENTRY_APPEND:
+		if (camel_file_util_decode_string (in, &entry->uid) == -1)
+			goto exception;
+		
+		break;
+	default:
+		goto exception;
+	}
+	
+	return (EDListNode *) entry;
+	
+ exception:
+	
+	switch (entry->type) {
+	case CAMEL_EXCHANGE_JOURNAL_ENTRY_APPEND:
+		g_free (entry->uid);
+		break;
+	default:
+		g_assert_not_reached ();
+	}
+	
+	g_free (entry);
+	
+	return NULL;
+}
+
+static int
+exchange_entry_write (CamelOfflineJournal *journal, EDListNode *entry, FILE *out)
+{
+	CamelExchangeJournalEntry *exchange_entry = (CamelExchangeJournalEntry *) entry;
+	
+	if (camel_file_util_encode_uint32 (out, exchange_entry->type) == -1)
+		return -1;
+	
+	switch (exchange_entry->type) {
+	case CAMEL_EXCHANGE_JOURNAL_ENTRY_APPEND:
+		if (camel_file_util_encode_string (out, exchange_entry->uid))
+			return -1;
+		
+		break;
+	default:
+		g_assert_not_reached ();
+	}
+	
+	return 0;
+}
+
+static int
+exchange_entry_play_append (CamelOfflineJournal *journal, CamelExchangeJournalEntry *entry, CamelException *ex)
+{
+	CamelExchangeFolder *exchange_folder = (CamelExchangeFolder *) journal->folder;
+	CamelFolder *folder = journal->folder;
+	CamelMimeMessage *message;
+	CamelMessageInfo *info;
+	CamelStream *stream;
+	CamelException lex;
+	
+	/* if the message isn't in the cache, the user went behind our backs so "not our problem" */
+	if (!exchange_folder->cache || !(stream = camel_data_cache_get (exchange_folder->cache, "cache", entry->uid, ex)))
+		goto done;
+	
+	message = camel_mime_message_new ();
+	if (camel_data_wrapper_construct_from_stream ((CamelDataWrapper *) message, stream) == -1) {
+		camel_object_unref (message);
+		camel_object_unref (stream);
+		goto done;
+	}
+	
+	camel_object_unref (stream);
+	
+	if (!(info = camel_folder_summary_uid (folder->summary, entry->uid))) {
+		/* info not in the summary, either because the summary
+		 * got corrupted or because the previous time this
+		 * journal was replay'd, it failed [1] */
+		info = camel_message_info_new (NULL);
+	}
+	
+	camel_exception_init (&lex);
+	camel_folder_append_message (folder, message, info, NULL, &lex);
+	camel_message_info_free (info);
+	camel_object_unref (message);
+	
+	if (camel_exception_is_set (&lex)) {
+		/* [1] remove the summary even if we fail or the next
+		 * summary downsync will break because info indexes
+		 * will be wrong
+		 *
+		 * FIXME: we really need to save these info's to a
+		 * temp location and then restore them after the
+		 * summary downsync finishes. */
+		camel_folder_summary_remove_uid (folder->summary, entry->uid);
+		camel_exception_xfer (ex, &lex);
+		return -1;
+	}
+	
+ done:
+	
+	camel_folder_summary_remove_uid (folder->summary, entry->uid);
+	camel_data_cache_remove (exchange_folder->cache, "cache", entry->uid, NULL);
+	
+	return 0;
+}
+
+static int
+exchange_entry_play (CamelOfflineJournal *journal, EDListNode *entry, CamelException *ex)
+{
+	CamelExchangeJournalEntry *exchange_entry = (CamelExchangeJournalEntry *) entry;
+	
+	switch (exchange_entry->type) {
+	case CAMEL_EXCHANGE_JOURNAL_ENTRY_APPEND:
+		return exchange_entry_play_append (journal, exchange_entry, ex);
+	default:
+		g_assert_not_reached ();
+		return -1;
+	}
+}
+
+
+
+CamelOfflineJournal *
+camel_exchange_journal_new (CamelExchangeFolder *folder, const char *filename)
+{
+	CamelOfflineJournal *journal;
+	
+	g_return_val_if_fail (CAMEL_IS_EXCHANGE_FOLDER (folder), NULL);
+	
+	journal = (CamelOfflineJournal *) camel_object_new (camel_exchange_journal_get_type ());
+	camel_offline_journal_construct (journal, (CamelFolder *) folder, filename);
+	
+	return journal;
+}
+
+
+void
+camel_exchange_journal_append (CamelExchangeJournal *exchange_journal, CamelMimeMessage *message,
+			    const CamelMessageInfo *mi, char **appended_uid, CamelException *ex)
+{
+	CamelOfflineJournal *journal = (CamelOfflineJournal *) exchange_journal;
+	CamelExchangeFolder *exchange_folder = (CamelExchangeFolder *) journal->folder;
+	CamelFolder *folder = (CamelFolder *) journal->folder;
+	CamelExchangeJournalEntry *entry;
+	CamelMessageInfoBase *a, *b;
+	CamelMessageInfo *info;
+	CamelStream *cache;
+	guint32 nextuid;
+	char *uid;
+	
+	if (exchange_folder->cache == NULL) {
+		camel_exception_set (ex, CAMEL_EXCEPTION_SYSTEM,
+				     _("Cannot append message in offline mode: cache unavailable"));
+		return;
+	}
+	
+	nextuid = camel_folder_summary_next_uid (folder->summary);
+	uid = g_strdup_printf ("-%u", nextuid);
+	
+	if (!(cache = camel_data_cache_add (exchange_folder->cache, "cache", uid, ex))) {
+		folder->summary->nextuid--;
+		g_free (uid);
+		return;
+	}
+	
+	if (camel_data_wrapper_write_to_stream ((CamelDataWrapper *) message, cache) == -1
+	    || camel_stream_flush (cache) == -1) {
+		camel_exception_setv (ex, CAMEL_EXCEPTION_SYSTEM,
+				      _("Cannot append message in offline mode: %s"),
+				      g_strerror (errno));
+		camel_data_cache_remove (exchange_folder->cache, "cache", uid, NULL);
+		folder->summary->nextuid--;
+		camel_object_unref (cache);
+		g_free (uid);
+		return;
+	}
+	
+	camel_object_unref (cache);
+	
+	entry = g_new (CamelExchangeJournalEntry, 1);
+	entry->type = CAMEL_EXCHANGE_JOURNAL_ENTRY_APPEND;
+	entry->uid = uid;
+	
+	e_dlist_addtail (&journal->queue, (EDListNode *) entry);
+	
+	info = camel_folder_summary_info_new_from_message (folder->summary, message);
+	info->uid = g_strdup (uid);
+	
+	a = (CamelMessageInfoBase *) info;
+	b = (CamelMessageInfoBase *) mi;
+	
+	camel_flag_list_copy (&a->user_flags, &b->user_flags);
+	camel_tag_list_copy (&a->user_tags, &b->user_tags);
+	a->date_received = b->date_received;
+	a->date_sent = b->date_sent;
+	a->flags = b->flags;
+	a->size = b->size;
+	
+	camel_folder_summary_add (folder->summary, info);
+	
+	if (appended_uid)
+		*appended_uid = g_strdup (uid);
+}
+
+void 
+camel_exchange_journal_transfer (CamelExchangeJournal *exchange_journal, CamelMimeMessage *message, const CamelMessageInfo *mi,
+				  char **transferred_uid, CamelException *ex)
+{
+	CamelOfflineJournal *journal = (CamelOfflineJournal *) exchange_journal;
+	CamelExchangeFolder *exchange_folder = (CamelExchangeFolder *) journal->folder;
+	CamelFolder *folder = (CamelFolder *) journal->folder;
+	CamelExchangeJournalEntry *entry;
+	CamelMessageInfoBase *a, *b;
+	CamelMessageInfo *info;
+	CamelStream *cache;
+	guint32 nextuid;
+	char *uid;
+	
+	if (exchange_folder->cache == NULL) {
+		camel_exception_set (ex, CAMEL_EXCEPTION_SYSTEM,
+				     _("Cannot append message in offline mode: cache unavailable"));
+		return;
+	}
+	
+	nextuid = camel_folder_summary_next_uid (folder->summary);
+	uid = g_strdup_printf ("-%u", nextuid);
+	
+	if (!(cache = camel_data_cache_add (exchange_folder->cache, "cache", uid, ex))) {
+		folder->summary->nextuid--;
+		g_free (uid);
+		return;
+	}
+	
+	if (camel_data_wrapper_write_to_stream ((CamelDataWrapper *) message, cache) == -1
+	    || camel_stream_flush (cache) == -1) {
+		camel_exception_setv (ex, CAMEL_EXCEPTION_SYSTEM,
+				      _("Cannot append message in offline mode: %s"),
+				      g_strerror (errno));
+		camel_data_cache_remove (exchange_folder->cache, "cache", uid, NULL);
+		folder->summary->nextuid--;
+		camel_object_unref (cache);
+		g_free (uid);
+		return;
+	}
+	
+	camel_object_unref (cache);
+	
+	entry = g_new (CamelExchangeJournalEntry, 1);
+	entry->type = CAMEL_EXCHANGE_JOURNAL_ENTRY_TRANSFER;
+	entry->uid = uid;
+	/* FIXME source and destination containers */
+
+	e_dlist_addtail (&journal->queue, (EDListNode *) entry);
+	
+	info = camel_folder_summary_info_new_from_message (folder->summary, message);
+	info->uid = g_strdup (uid);
+	
+	a = (CamelMessageInfoBase *) info;
+	b = (CamelMessageInfoBase *) mi;
+	
+	camel_flag_list_copy (&a->user_flags, &b->user_flags);
+	camel_tag_list_copy (&a->user_tags, &b->user_tags);
+	a->date_received = b->date_received;
+	a->date_sent = b->date_sent;
+	a->flags = b->flags;
+	a->size = b->size;
+	
+	camel_folder_summary_add (folder->summary, info);
+	
+	if (transferred_uid)
+		*transferred_uid = g_strdup (uid);	
+}
+
---
+++ camel/camel-exchange-journal.h
@@ -0,0 +1,92 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ *  Copyright 2004 Novell, Inc. (www.novell.com)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+
+#ifndef __CAMEL_EXCHANGE_JOURNAL_H__
+#define __CAMEL_EXCHANGE_JOURNAL_H__
+
+#include <stdarg.h>
+
+#include <glib.h>
+
+#include <camel/camel-offline-journal.h>
+#include <camel/camel-mime-message.h>
+#include "camel-exchange-folder.h"
+
+#ifdef __cplusplus
+extern "C" {
+#pragma }
+#endif /* __cplusplus */
+
+#define CAMEL_TYPE_EXCHANGE_JOURNAL            (camel_exchange_journal_get_type ())
+#define CAMEL_EXCHANGE_JOURNAL(obj)            (CAMEL_CHECK_CAST ((obj), CAMEL_TYPE_EXCHANGE_JOURNAL, CamelExchangeJournal))
+#define CAMEL_EXCHANGE_JOURNAL_CLASS(klass)    (CAMEL_CHECK_CLASS_CAST ((klass), CAMEL_TYPE_EXCHANGE_JOURNAL, CamelExchangeJournalClass))
+#define CAMEL_IS_EXCHANGE_JOURNAL(obj)         (CAMEL_CHECK_TYPE ((obj), CAMEL_TYPE_EXCHANGE_JOURNAL))
+#define CAMEL_IS_EXCHANGE_JOURNAL_CLASS(klass) (CAMEL_CHECK_CLASS_TYPE ((klass), CAMEL_TYPE_EXCHANGE_JOURNAL))
+#define CAMEL_EXCHANGE_JOURNAL_GET_CLASS(obj)  (CAMEL_CHECK_GET_CLASS ((obj), CAMEL_TYPE_EXCHANGE_JOURNAL, CamelExchangeJournalClass))
+
+typedef struct _CamelExchangeJournal CamelExchangeJournal;
+typedef struct _CamelExchangeJournalClass CamelExchangeJournalClass;
+typedef struct _CamelExchangeJournalEntry CamelExchangeJournalEntry;
+
+//struct CamelExchangeFolder;
+
+enum {
+	CAMEL_EXCHANGE_JOURNAL_ENTRY_APPEND,
+	CAMEL_EXCHANGE_JOURNAL_ENTRY_TRANSFER
+};
+
+struct _CamelExchangeJournalEntry {
+	EDListNode node;
+	
+	int type;
+
+	char *uid;
+	char *source_container;
+	char *dest_container;
+};
+
+struct _CamelExchangeJournal {
+	CamelOfflineJournal parent_object;
+	
+};
+
+struct _CamelExchangeJournalClass {
+	CamelOfflineJournalClass parent_class;
+	
+};
+
+
+CamelType camel_exchange_journal_get_type (void);
+
+CamelOfflineJournal *camel_exchange_journal_new (CamelExchangeFolder *folder, const char *filename);
+
+/* interfaces for adding a journal entry */
+void camel_exchange_journal_append (CamelExchangeJournal *journal, CamelMimeMessage *message, 
+				const CamelMessageInfo *mi, char **appended_uid, CamelException *ex);
+
+void camel_exchange_journal_transfer (CamelExchangeJournal *journal, CamelMimeMessage *message, 
+				const CamelMessageInfo *mi, char **transferred_uid, CamelException *ex);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* __CAMEL_EXCHANGE_JOURNAL_H__ */
