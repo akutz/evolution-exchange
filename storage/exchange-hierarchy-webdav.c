42c27722c088e592cf93670329650308311ab410
Sarfraaz Ahmed <asarfraaz@novell.com>
2005-01-29 07:27:41 +0000

    Check for duplicate entried being added into the hashtable. Fixes #68330
    2005-01-29  Sarfraaz Ahmed <asarfraaz@novell.com>
    
            * storage/exchange-hierarchy-webdav.c (hierarchy_new_folder):
            Check for duplicate entried being added into the hashtable.
            Fixes #68330

--- storage/exchange-hierarchy-webdav.c
+++ storage/exchange-hierarchy-webdav.c
@@ -186,6 +186,13 @@ hierarchy_new_folder (ExchangeHierarchy *hier, EFolder *folder,
 	g_return_if_fail (E_IS_FOLDER (folder));
 	internal_uri = e_folder_exchange_get_internal_uri (folder);
 
+	/* This should ideally not be needed. But, this causes a problem when the
+	server has identical folder names [ internal_uri ] for folders. Very much
+	possible in the case of favorite folders */
+	if (g_hash_table_lookup (EXCHANGE_HIERARCHY_WEBDAV (hier)->priv->folders_by_internal_path,
+				(char *)e2k_uri_path (internal_uri)))
+		return;
+
 	g_hash_table_insert (EXCHANGE_HIERARCHY_WEBDAV (hier)->priv->folders_by_internal_path,
 			     (char *)e2k_uri_path (internal_uri), folder);
 
