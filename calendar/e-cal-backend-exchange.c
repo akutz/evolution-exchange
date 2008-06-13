6c7665de95aa84107e6ab91d3111df611b274baf
Milan Crha <mcrha@redhat.com>
2008-06-13 06:22:58 +0000

    ** Fix for bug #402752
    2008-06-13  Milan Crha  <mcrha@redhat.com>
    
    	** Fix for bug #402752
    
    	* calendar/e-cal-backend-exchange.c: (get_exchange_comp),
    	(e_cal_backend_exchange_add_object),
    	(e_cal_backend_exchange_modify_object):
    	* calendar/e-cal-backend-exchange-calendar.c: (add_ical):
    	Prevent crash when got a component without set UID.
    
    
    svn path=/trunk/; revision=1683

--- calendar/e-cal-backend-exchange.c
+++ calendar/e-cal-backend-exchange.c
@@ -642,6 +642,9 @@ e_cal_backend_exchange_add_object (ECalBackendExchange *cbex,
 	d(printf("ecbe_add_object(%p, %s, %s)\n", cbex, href, lastmod));
 
 	uid = icalcomponent_get_uid (comp);
+	if (!uid)
+		return FALSE;
+
 	ecomp = g_hash_table_lookup (cbex->priv->objects, uid);
 
 	is_instance = (icalcomponent_get_first_property (comp, ICAL_RECURRENCEID_PROPERTY) != NULL);
@@ -742,6 +745,9 @@ e_cal_backend_exchange_modify_object (ECalBackendExchange *cbex,
 			      FALSE);
 
 	uid = icalcomponent_get_uid (comp);
+	if (!uid)
+		return FALSE;
+
 	rid = icalcomponent_get_recurrenceid (comp);
 
 	ecomp = g_hash_table_lookup (cbex->priv->objects, uid);
@@ -828,6 +834,9 @@ ECalBackendExchangeComponent *
 get_exchange_comp (ECalBackendExchange *cbex, const char *uid)
 {
 	ECalBackendExchangeComponent *ecomp;
+
+	if (!uid)
+		return NULL;
 
 	ecomp = g_hash_table_lookup (cbex->priv->objects, uid);
 	if (ecomp)
