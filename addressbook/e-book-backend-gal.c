83b5432ea82fe73d0fe0898d057662ebba3e57d3
Matthew Barnes <mbarnes@redhat.com>
2010-05-01 18:12:41 -0400

    Coding style cleanup.

--- addressbook/e-book-backend-gal.c
+++ addressbook/e-book-backend-gal.c
@@ -2250,7 +2250,7 @@ getNextPage:
 		}
 		ber_printf( prber, "{iO}", pageSize, &cookie );
 #ifdef G_OS_WIN32
-		if ( ber_flatten( prber, tmpBVPtr) == -1 ) {
+		if (ber_flatten( prber, tmpBVPtr) == -1) {
 			ber_free( prber, 1 );
 			ber_bvfree(*tmpBVPtr);
 			return;
@@ -2259,7 +2259,7 @@ getNextPage:
 		ber_free( prber, 1 );
 		ber_bvfree(*tmpBVPtr);
 #else
-		if ( ber_flatten2( prber, &c[i].ldctl_value, 0 ) == -1 ) {
+		if (ber_flatten2( prber, &c[i].ldctl_value, 0 ) == -1) {
 			return;
 		}
 #endif
